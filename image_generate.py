09fbef91901afefc9a7eaa8ae52cf60775d342d4
arpa2001 <arpajitofficial@gmail.com>
2021-04-21 23:04:14 +0530

    Files added

---
+++ image_generate.py
@@ -0,0 +1,16 @@
+# ...include code from https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
+
+import shap
+import numpy as np
+
+# select a set of background examples to take an expectation over
+background = x_train[np.random.choice(x_train.shape[0], 100, replace=False)]
+
+# explain predictions of the model on three images
+e = shap.DeepExplainer(model, background)
+# ...or pass tensors directly
+# e = shap.DeepExplainer((model.layers[0].input, model.layers[-1].output), background)
+shap_values = e.shap_values(x_test[1:5])
+
+# plot the feature attributions
+shap.image_plot(shap_values, -x_test[1:5])
